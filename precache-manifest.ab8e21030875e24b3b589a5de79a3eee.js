f4be4b4a5005615f2d2e4c506bcdbf19a44d82d2
Varga Péter <iampropeter@gmail.com>
2020-05-17 12:28:22 +0200

    Updates

---
+++ precache-manifest.ab8e21030875e24b3b589a5de79a3eee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addd515f4752143bfa5f69041374912d",
+    "url": "/chrypulus/index.html"
+  },
+  {
+    "revision": "7fcc2c57aa9d06223c57",
+    "url": "/chrypulus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8939ff17dc965f1971de",
+    "url": "/chrypulus/static/js/2.49aa9113.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chrypulus/static/js/2.49aa9113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcc2c57aa9d06223c57",
+    "url": "/chrypulus/static/js/main.2286a996.chunk.js"
+  },
+  {
+    "revision": "8a44f31f098b47d872e5",
+    "url": "/chrypulus/static/js/runtime-main.78521d32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chrypulus/static/media/logo.5d5d9eef.svg"
+  }
+]);
